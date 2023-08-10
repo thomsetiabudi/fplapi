2996e5283426fdcc153cd9ff377a9c9129dead6d
thomsetiabudi <thomsetiabudi@gmail.com>
2023-08-10 07:22:46 +0700

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -68,7 +68,6 @@ def app():
                 arr = np.random.normal(1, 1, size=100)
                 fig, ax = plt.subplots()
                 ax.hist(arr, bins=20)
-
                 st.pyplot(fig)
 
 
