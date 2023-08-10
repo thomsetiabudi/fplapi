0d668304af61d7a064b38294daad9b85ea0a5741
thomsetiabudi <thomsetiabudi@gmail.com>
2023-08-10 07:54:58 +0700

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -68,7 +68,7 @@ def app():
                 x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
                 y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
                 fig = plt.scatter(x, y)
-                # st.pyplot(fig)
+                st.pyplot(fig)
 
 
 if __name__ == '__main__':
